04f19b71dc8c59c1f141c2c6ad6dc5a8a0ede655
ReinKaos95 <malcom2209@hotmail.com>
2021-08-19 10:18:15 -0400

    Instalando vue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 @if(Auth::User()->us_rol_id==1)
 <h1 class="fw-bold">Bienvenido! {{Auth::user()->usuario}}</h1>
 	<p>Este es un modal de administrador</p>
+	
 	@endif
 
 	@if(Auth::User()->us_rol_id==2)
